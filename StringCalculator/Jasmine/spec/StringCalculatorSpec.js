1ec9ea79961b25679352907dc1c17b31916a3a76
Mlungisi <mgmgwala@gmail.com>
2019-08-28 11:35:38 +0200

    .

--- StringCalculator/Jasmine/spec/StringCalculatorSpec.js
+++ StringCalculator/Jasmine/spec/StringCalculatorSpec.js
@@ -1,11 +1,14 @@
 describe("Add", function(){
 	it("should return 0 when there are is a string of length 0/no arguments.", function(){
-		expect(add("")).toEqual(0);
+		expect(add("")).toBe(0);
 	});
 	it("should return a single number as it is.", function(){
-		expect(add("309")).toEqual(309);
+		expect(add("309")).toBe(309);
 	});
 	it("should return the sum of two numbers.", function(){
-		expect(add("309,21")).toEqual(330);
+		expect(add("309,21")).toBe(330);
+	});
+	it("should handle an unknown amount of numbers.", function(){
+		expect(add("5,5,5,5,5,5,5")).toBe(35);
 	});
 });
--- StringCalculator/Jasmine/src/stringCalculator.js
+++ StringCalculator/Jasmine/src/stringCalculator.js
@@ -1,37 +1,14 @@
 function add(str)
 {
-	var arrNum = [];
-	var i = 0;
-	var num1 = 0;
-	var num2 = 0;
-	if (arguments.length === 0 || arguments[0] == "")
-		return (0);
-	else
-	{
-		while (i < str.length)
-		{
-			while (str[i] != ',')
-			{
-				arrNum.push(str[i++]);
-				if (str[i + 1] === ',')
-				{
-					num1 = Number(arrNum.join(""));
-					arrNum = [];
-					i++;
-					i++;
-					break ;
-				}
-			}
-		//	if (str[i] === ',' || str[i] === ' ')
-		//		i++;
-			arrNum.push(str[i++]);
-		}
-		console.log(arrNum);
-		num2 = Number(arrNum.join(""));
-		console.log(num2);
-		return(num1 + num2);
-		
-	}
+	console.log(s);
+        var i = 0;
+        var sum = 0;
+        var arr = str.split(',');
+        while (i < arr.length)
+        {
+                var num = arr[i++];
+                num = Number(num);
+                sum = sum + num;
+        }
+        return (sum);
 }
-
-add("309,21");
